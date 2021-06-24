3049c56e6ac24ea91cb54d9db89c91747348f1de
george murphy <georgeolivermurphy@hotmail.com>
2021-06-24 17:18:04 +0100

    smooth scroll js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,7 +1,14 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+// const channels = require.context('.', true, /_channel\.js$/)
+// channels.keys().forEach(channels)
 
+// function smoothScroll(target,duration) {
+//   var target = document.querySelector(target);
+//   var targetPosition = target.getBoundingClientRect();
 
+//   console.log(targetPosition);
+// }
+
+// smoothScroll('.section1', 1000);
